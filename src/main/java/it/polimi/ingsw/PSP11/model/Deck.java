99cfd639afd043043525069db17930ce5ccc2719
AndreaFigini <10574456@polimi.it>
2020-03-27 18:42:12 +0100

    add the class Deck

--- src/main/java/it/polimi/ingsw/PSP11/model/Deck.java
+++ src/main/java/it/polimi/ingsw/PSP11/model/Deck.java
@@ -1,5 +1,34 @@
 package it.polimi.ingsw.PSP11.model;
 
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    /**
+     * class constructor
+     */
+    public Deck(ArrayList cards){
+        cards = new ArrayList<Card>();
+    }
+
+
+    /**
+     * add a card to the deck
+     * @param card card that will be added to the deck
+     */
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    /**
+     * return the card with the index i
+     * @param i index of the card that get returned
+     * @return the card with the index i
+     */
+    public Card pickGod(int i){
+        return cards.get(i);
+    }
 
 }
