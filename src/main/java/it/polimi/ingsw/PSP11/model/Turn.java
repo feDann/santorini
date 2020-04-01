95ff4d3ce7d3b5f6e7b0e9cac19babaeab1f69eb
feDann <daniele99@ferrazzo.it>
2020-04-01 18:50:20 +0200

    add javadoc of Turn interface

--- src/main/java/it/polimi/ingsw/PSP11/model/Turn.java
+++ src/main/java/it/polimi/ingsw/PSP11/model/Turn.java
@@ -5,14 +5,45 @@ import java.util.ArrayList;
 
 public interface Turn {
 
+    /**
+     *Method that looks for possible moves
+     * @param worker the selected worker
+     * @param board the game board
+     * @return an array list of possible move position
+     */
+
     public ArrayList<Point> move(Worker worker, Board board);
 
-    public void applyMove(Worker worker, Board board, Point point);
+    /**
+     *Method that move the worker in the newPosition
+     * @param worker the selected worker
+     * @param board the game board
+     * @param newPosition the new worker position
+     */
+    public void applyMove(Worker worker, Board board, Point newPosition);
 
+    /**
+     *Method that looks for possible build position
+     * @param worker the selected worker
+     * @param board the game board
+     * @return an array list of possible build position
+     */
     public ArrayList<Point> build(Worker worker, Board board);
 
-    public void applyBuild(Worker worker, Board board, Point point);
+    /**
+     *Method that build a block or dome in the buildPosition
+     * @param worker the selected worker
+     * @param board the game board
+     * @param buildPosition the build position
+     */
+    public void applyBuild(Worker worker, Board board, Point buildPosition);
 
+    /**
+     *Method that check if a player's worker won
+     * @param worker the selected worker
+     * @param board the game board
+     * @return true if player won, false otherwise
+     */
     public boolean winCondition(Worker worker, Board board);
 
 }
