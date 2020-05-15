73535042a61f020d6b9a7736c2889c2501b78e87
feDann <daniele99@ferrazzo.it>
2020-05-15 20:08:29 +0200

    implement scene of GUI for StartTurnState, MoveState and BuildState

--- src/main/java/it/polimi/ingsw/PSP11/view/gui/controller/GameSceneController.java
+++ src/main/java/it/polimi/ingsw/PSP11/view/gui/controller/GameSceneController.java
@@ -5,9 +5,11 @@ import it.polimi.ingsw.PSP11.messages.*;
 import it.polimi.ingsw.PSP11.model.Board;
 
 import it.polimi.ingsw.PSP11.model.Color;
+import it.polimi.ingsw.PSP11.model.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.application.Platform;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
@@ -15,8 +17,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GameSceneController extends GUIController {
 
@@ -36,6 +40,7 @@ public class GameSceneController extends GUIController {
         imagePane.setVisible(true);
 //        imageGrid.setVisible(true);
         initializeActionGridButtons();
+        initializeImageGrid();
         actionPane.setVisible(false);
         serverLog.setWrapText(true);
         serverLog.setEditable(false);
@@ -54,6 +59,40 @@ public class GameSceneController extends GUIController {
     }
 
 
+
+    @FXML
+    public void selectWorker(ActionEvent event){
+        Button button = (Button)event.getSource();
+        getClient().asyncWrite(new SelectWorkerResponse(Integer.parseInt(button.getId())));
+        actionPane.setVisible(false);
+        //TODO button.setOnAction(null)
+    }
+
+    @FXML
+    public void moveWorker(ActionEvent event) {
+        Platform.runLater(() -> {
+            Button button = (Button) event.getSource();
+            String[] cord = button.getId().split(",");
+            Point position = new Point(Integer.parseInt(cord[0]), Integer.parseInt(cord[1]));
+            getClient().asyncWrite(new MoveResponse(position));
+            actionPane.setVisible(false);
+
+        });
+    }
+
+    @FXML
+    public void buildBlock(ActionEvent event) {
+        Platform.runLater(() -> {
+            Button button = (Button) event.getSource();
+            String[] cord = button.getId().split(",");
+            Point position = new Point(Integer.parseInt(cord[0]), Integer.parseInt(cord[1]));
+            getClient().asyncWrite(new BuildResponse(position));
+            actionPane.setVisible(false);
+
+        });
+    }
+
+
     public void initializeActionGridButtons(){
         for(int x = 0;x <5;x++){
             for(int y = 0; y<5; y++){
@@ -67,6 +106,26 @@ public class GameSceneController extends GUIController {
             }
         }
     }
+
+    public void initializeImageGrid(){
+        for(int x = 0;x <5;x++){
+            for(int y = 0; y<5; y++){
+                StackPane stackpane = new StackPane();
+                stackpane.setPrefWidth(imageGrid.getPrefWidth()/5);
+                stackpane.setPrefHeight(imageGrid.getPrefHeight()/5);
+                stackpane.setStyle("-fx-background-color: transparent");
+                stackpane.setVisible(true);
+                imageGrid.add(stackpane,y,x);
+            }
+        }
+    }
+
+    public void clearStackPanes(){
+        for(Node node: imageGrid.getChildren()){
+            ((StackPane) node).getChildren().clear();
+        }
+    }
+
 
 
 
@@ -93,18 +152,123 @@ public class GameSceneController extends GUIController {
     }
 
 
+    public void setAllInvisible(GridPane pane){
+        for(Node n : pane.getChildren()){
+            n.setVisible(false);
+        }
+    }
 
-    public void updateBoard(Board board){
+    public void selectWorkerView(ArrayList<Worker> workers){
         Platform.runLater(()->{
+            actionPane.setVisible(true);
+            setAllInvisible(actionGrid);
+            for(Worker worker : workers){
+                Point workerPosition = worker.getPosition();
+                Button button = (Button )actionGrid.getChildren().get(workerPosition.x * 5 + workerPosition.y);
+                button.setId(String.valueOf(worker.getId()));
+                button.setOnAction(this::selectWorker);
+                button.setVisible(true);
+            }
+        });
+
+    }
+
+    public void moveView(ArrayList<Point> possibleMoves){
+
+        Platform.runLater(()->{
+            actionPane.setVisible(true);
+            setAllInvisible(actionGrid);
+            for(Point position : possibleMoves){
+                Button button = (Button )actionGrid.getChildren().get(position.x * 5 + position.y);
+                button.setId(position.x +","+position.y);
+                button.setOnAction(this::moveWorker);
+                button.setVisible(true);
+            }
+        });
+
+    }
+
+    public void buildView(ArrayList<Point> possibleBuilds){
+
+        Platform.runLater(()->{
+            actionPane.setVisible(true);
+            setAllInvisible(actionGrid);
+            for(Point position : possibleBuilds){
+                Button button = (Button )actionGrid.getChildren().get(position.x * 5 + position.y);
+                button.setId(position.x +","+position.y);
+                button.setOnAction(this::buildBlock);
+                button.setVisible(true);
+            }
+        });
+
+    }
+
+
+
+
+    public void updateBoardView(Board board){
+        Platform.runLater( ()->{
+            clearStackPanes();
             for(int x = 0; x <5 ; x++){
                 for(int y = 0; y<5; y++){
-                    if(board.hasWorkerOnTop(new Point(x,y))){
-                        ImageView image = new ImageView();
-                        image.setImage(new Image(getClass().getResource(chooseWorker(board.getWorker(new Point(x,y)).getColor())).toString()));
-                        image.setFitWidth(imageGrid.getPrefWidth()/5);
-                        image.setFitHeight(imageGrid.getPrefHeight()/5);
-                        imageGrid.add(image,y,x);
+                    Point position = new Point(x,y);
+                    StackPane stack = ((StackPane)imageGrid.getChildren().get(x*5+y));
+
+                    //set the blocks
+                    if(board.getCurrentLevel(position).ordinal() == 1){
+                        ImageView baseBlock = new ImageView();
+                        baseBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        baseBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        baseBlock.setImage(new Image(getClass().getResource("/images/blocks/base_block.png").toString()));
+                        stack.getChildren().add(baseBlock);
+                    }else if(board.getCurrentLevel(position).ordinal() == 2){
+                        ImageView baseBlock = new ImageView();
+                        baseBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        baseBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        baseBlock.setImage(new Image(getClass().getResource("/images/blocks/base_block.png").toString()));
+                        ImageView middleBlock = new ImageView();
+                        middleBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        middleBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        middleBlock.setImage(new Image(getClass().getResource("/images/blocks/mid_block.png").toString()));
+                        stack.getChildren().add(baseBlock);
+                        stack.getChildren().add(middleBlock);
                     }
+                    else if(board.getCurrentLevel(position).ordinal() == 3){
+                        ImageView baseBlock = new ImageView();
+                        baseBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        baseBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        baseBlock.setImage(new Image(getClass().getResource("/images/blocks/base_block.png").toString()));
+                        ImageView middleBlock = new ImageView();
+                        middleBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        middleBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        middleBlock.setImage(new Image(getClass().getResource("/images/blocks/mid_block.png").toString()));
+                        ImageView topBlock = new ImageView();
+                        topBlock.setFitWidth(imageGrid.getPrefWidth()/5);
+                        topBlock.setFitHeight(imageGrid.getPrefHeight()/5);
+                        topBlock.setImage(new Image(getClass().getResource("/images/blocks/top_block.png").toString()));
+                        stack.getChildren().add(baseBlock);
+                        stack.getChildren().add(middleBlock);
+                        stack.getChildren().add(topBlock);
+                    }
+
+
+
+
+                    if(board.hasWorkerOnTop(position)){
+                        ImageView workerImage = new ImageView();
+                        workerImage.setFitWidth(imageGrid.getPrefWidth()/5);
+                        workerImage.setFitHeight(imageGrid.getPrefHeight()/5);
+                        workerImage.setImage(new Image(getClass().getResource(chooseWorker(board.getWorker(position).getColor())).toString()));
+                        stack.getChildren().add(workerImage);
+                    }
+                    else if(board.hasDomeOnTop(position)){
+                        ImageView dome = new ImageView();
+                        dome.setFitWidth(imageGrid.getPrefWidth()/5);
+                        dome.setFitHeight(imageGrid.getPrefHeight()/5);
+                        dome.setImage(new Image(getClass().getResource("/images/blocks/dome.png").toString()));
+                        stack.getChildren().add(dome);
+                    }
+
                 }
             }
         });
@@ -129,10 +293,19 @@ public class GameSceneController extends GUIController {
             placeWorker();
         }
         else if(message instanceof BoardUpdate){
-            updateBoard(((BoardUpdate) message).getBoard());
+            updateBoardView(((BoardUpdate) message).getBoard());
         }
         else if(message instanceof EndTurnMessage){
             //TODO view per End Turn
+        }
+        else if (message instanceof SelectWorkerRequest){
+            selectWorkerView(((SelectWorkerRequest) message).getAvailableWorkers());
+        }
+        else if (message instanceof MoveRequest){
+            moveView(((MoveRequest) message).getPossibleMoves());
+        }
+        else if(message instanceof BuildRequest){
+            buildView(((BuildRequest) message).getPossibleBuilds());
         }
         else if(message instanceof InvalidWorkerPosition){
             serverLog.appendText("[SERVER]: " + formatString(message.getMessage()) +"\n");
