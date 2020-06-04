952987107739c4688e1c8715fe906d89724057fb
feDann <daniele99@ferrazzo.it>
2020-06-04 19:21:02 +0200

    remove TooManyPeopleMessage

--- src/main/java/it/polimi/ingsw/PSP11/messages/TooManyPeopleMessage.java
+++
@@ -1,7 +0,0 @@
-package it.polimi.ingsw.PSP11.messages;
-
-public class TooManyPeopleMessage extends SimpleMessage{
-    public TooManyPeopleMessage() {
-        super("Too many people in the lobby, find other friends!\n");
-    }
-}
