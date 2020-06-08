9caa85d6bf502aaf70618f4e7b6fb229a7194c85
feDann <daniele99@ferrazzo.it>
2020-06-08 16:43:28 +0200

    update javadoc

--- src/main/java/it/polimi/ingsw/PSP11/utils/TimeStamp.java
+++ src/main/java/it/polimi/ingsw/PSP11/utils/TimeStamp.java
@@ -5,6 +5,10 @@ import java.util.Calendar;
 
 public class TimeStamp {
 
+    /**
+     *
+     * @return the a string representing the current day and time formatted as [yyyy-mm-dd_hh:mm:ss]
+     */
     public static String getTimeSTamp(){
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
         return "[" + timeStamp +"] ";
